c4c845b331a8addc7e4a612daad667aed5ae4f1e
orira22 <oriroitzaid@gmail.com>
2022-12-22 15:26:39 +0200

    Final commit - a button that changes its text when you click on it

--- app/src/main/java/com/example/appex031/MainActivity.java
+++ app/src/main/java/com/example/appex031/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.appex031;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+    }
+
+    public void changeText(View view) {
+        btn.setText("Oh, yea, I’ve been clicked!");
     }
 }
